6acfc71d7b5430b05b5157a26000bffe3d9d7667
sebastian Högsander <seblhog@gmail.com>
2024-10-16 13:22:52 +0200

    create user

--- event-finder-backend/index.js
+++ event-finder-backend/index.js
@@ -1,29 +1,37 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+import { createUsers } from './seed/createUser.js';
+import express from 'express';
+import mongoose from 'mongoose';
+import User from './models/userSchema.js';
 
-import  {createUsers}  from "./seed/createUser.js";
-import express from "express";
-import { MongoClient } from "mongodb";
-import mongoose from "mongoose";
 const app = express();
+app.use(express.json());
+dotenv.config();
 const connectionString = process.env.MONGODB_URL;
-console.log(connectionString);
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, () => console.log('Server is running'));
 
-
-
-mongoose.connect(connectionString)
-.then(() => {
-    console.log('Connected to MongoDB');
-    createUsers();
-})
-.catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
-
+mongoose
+	.connect(connectionString)
+	.then(() => {
+		console.log('Connected to MongoDB');
+		// createUsers();
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error);
+	});
 mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to db');
+	console.log('Mongoose connected to db');
 });
 
+app.post('/users', async (req, res) => {
+	const { email, password, username, age } = req.body;
+	const newUser = new User({
+		email,
+		password,
+		username,
+		age,
+	});
 
-
+	await newUser.save();
+	res.status(200).json([{ MESSAGE: 'CREATED NEW USER', newUser }]);
+});
--- event-finder-backend/seed/createUser.js
+++ event-finder-backend/seed/createUser.js
@@ -1,22 +1,19 @@
 import User from '../models/userSchema.js';
 
 export const createUsers = async () => {
-    console.log("Creating users");
-    const newUser = new User({
-        email: "test@mail.com",
-        password: "password",
-        username: "test",
-        age: 20,
-        ignoredEvents: [],
-        isOrganizer: false,
-        createdEvents: [],
-        joinedEvents: [],
-        awaitingApprovalEvents: [],
-    });
-    await newUser.save().then(() => {
-        console.log("User created");
-    });
-}
-
-
-
+	console.log('Creating users');
+	const newUser = new User({
+		email: 'test@mail.com',
+		password: 'password',
+		username: 'test',
+		age: 20,
+		ignoredEvents: [],
+		isOrganizer: false,
+		createdEvents: [],
+		joinedEvents: [],
+		awaitingApprovalEvents: [],
+	});
+	await newUser.save().then(() => {
+		console.log('User created');
+	});
+};
