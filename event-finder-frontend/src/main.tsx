2559874d41e2272f0f51459ef2d5615234f83409
DennisFlink <dennis.sabriev@outlook.com>
2024-10-18 12:20:50 +0200

    Refactor import statements in routes.js and users.js

--- event-finder-frontend/src/main.tsx
+++ event-finder-frontend/src/main.tsx
@@ -3,18 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { LoginView } from './views/loginView/loginView.tsx';
 
 const router = createBrowserRouter([
    {
       path: '/',
       element: <App />,
 
-      children: [
-         {
-            path: 'login',
-            element: <div>hej</div>,
-         },
-      ],
+      children: [],
+   },
+   {
+      path: '/login',
+      element: <LoginView />,
    },
 ]);
 
