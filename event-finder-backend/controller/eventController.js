e27aa74802e10fee47fc13740a958fe526cbfa80
skoanton <antonhakansonn@gmail.com>
2024-10-18 16:24:36 +0200

    start of filter

--- event-finder-backend/controller/eventController.js
+++ event-finder-backend/controller/eventController.js
@@ -3,7 +3,7 @@ import { createEvent } from '../services/eventService.js';
 const createEventController = async (req, res) => {
   try {
     const event = await createEvent(req.body);
-    res.status(201).json(event); 
+    res.status(201).json(event);
   } catch (error) {
     res.status(500).json({ error: 'Failed to create event', message: error.message });
   }
