ed5dc8d8507feaa7950469f68b0374cb3f01be1b
sebastian Högsander <seblhog@gmail.com>
2024-10-18 14:41:57 +0200

    created events view

--- event-finder-frontend/src/components/Header.tsx
+++ event-finder-frontend/src/components/Header.tsx
@@ -1,75 +1,79 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { ArrowLeft } from "lucide-react";
-import { Button } from "./ui/button";
-import CreateEventModal from "./CreateEventModal";
-import { Link, useLocation } from "react-router-dom";
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ArrowLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import CreateEventModal from './CreateEventModal';
+import { Link, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 type HeaderProps = {
-  // TODO
-  // import users,
+	// TODO
+	// import users,
 };
 
 export default function Header({}: HeaderProps) {
-  const location = useLocation();
-  const handleLogout = () => {
-    console.log("logging out...");
-  };
+	const location = useLocation();
+	const navigate = useNavigate();
+	const handleLogout = () => {
+		console.log('logging out...');
+	};
 
-  const handleCreateEvent = () => {
-    console.log("creating event...");
-    location.pathname === "/events/create";
-  };
+	const handleCreateEvent = () => {
+		console.log('creating event...');
+	};
 
-  const handleSeeEvents = () => {
-    console.log("seeing events...");
-  };
+	const handleSeeEvents = () => {
+		console.log('seeing events...');
+		navigate('/createdEvents');
+	};
 
-  const handleGoBack = () => {
-    console.log("going back...");
-  };
+	const handleGoBack = () => {
+		console.log('going back...');
+	};
 
-  return (
-    <>
-      <header className="p-5 shadow-lg flex flex justify-between items-center">
-        {/* visa endast om current page inte är home */}
-        <Button variant={"ghost"} onClick={() => handleGoBack()}>
-          <ArrowLeft />
-        </Button>
+	return (
+		<>
+			<header className="p-5 shadow-lg flex flex justify-between items-center">
+				{/* visa endast om current page inte är home */}
+				<Button
+					variant={'ghost'}
+					onClick={() => handleGoBack()}>
+					<ArrowLeft />
+				</Button>
 
-        <div className="flex gap-3 items-center justify-end">
-          <p className="font-bold">skoanton</p>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Link to="/events/create">Create new event</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => handleSeeEvents()}>
-                See your events
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => handleLogout()}>
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </header>
-    </>
-  );
+				<div className="flex gap-3 items-center justify-end">
+					<p className="font-bold">skoanton</p>
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+							<Avatar>
+								<AvatarImage src="https://github.com/shadcn.png" />
+								<AvatarFallback>CN</AvatarFallback>
+							</Avatar>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuLabel>My account</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>
+								<Link to="/events/create">Create new event</Link>
+							</DropdownMenuItem>
+							<DropdownMenuItem onClick={() => handleSeeEvents()}>
+								Your events
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem onClick={() => handleLogout()}>
+								Logout
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			</header>
+		</>
+	);
 }
--- event-finder-frontend/src/views/OwnEvents/OwnEvents.tsx
+++ event-finder-frontend/src/views/OwnEvents/OwnEvents.tsx
@@ -1,40 +1,47 @@
 import axios from 'axios';
+import { useState } from 'react';
 import {
 	Accordion,
 	AccordionContent,
 	AccordionItem,
 	AccordionTrigger,
 } from '@/components/ui/accordion';
+import IEvent from 'interface/eventTypes';
 
 export default function OwnEvents() {
+	const [userEvents, setUserEvents] = useState<IEvent[]>([]);
+
 	const getCreatedEvents = async () => {
-		const userEvents = await axios.get(
-			`http://localhost:3000/api/events/author/0v5t6mgnw83v03n65w8g7`,
-		);
-		console.log(userEvents.data);
+		try {
+			const response = await axios.get(
+				`http://localhost:3000/api/events/author/0v5t6mgnw83v03n65w8g7`,
+			);
+			setUserEvents(response.data);
+		} catch (e) {
+			console.error('Error fetching events:', e);
+		}
 	};
+
 	getCreatedEvents();
 
 	return (
 		<Accordion
 			type="single"
 			collapsible>
-			<AccordionItem value="item-1">
-				<AccordionTrigger>Created event 1</AccordionTrigger>
-				<AccordionContent>event info</AccordionContent>
-			</AccordionItem>
-			<AccordionItem value="item-2">
-				<AccordionTrigger>Created event 2</AccordionTrigger>
-				<AccordionContent>event info</AccordionContent>
-			</AccordionItem>
-			<AccordionItem value="item-3">
-				<AccordionTrigger>Created event 3</AccordionTrigger>
-				<AccordionContent>event info</AccordionContent>
-			</AccordionItem>
-			<AccordionItem value="item-4">
-				<AccordionTrigger>Created event 4</AccordionTrigger>
-				<AccordionContent>event info</AccordionContent>
-			</AccordionItem>
+			{userEvents.length > 0 ? (
+				userEvents.map((event, index) => (
+					<AccordionItem
+						key={index}
+						value={`item-${index}`}>
+						<AccordionTrigger>{event.title}</AccordionTrigger>
+						<AccordionContent>
+							<div className="bg-red-700"></div>
+						</AccordionContent>
+					</AccordionItem>
+				))
+			) : (
+				<p>No events found.</p>
+			)}
 		</Accordion>
 	);
 }
