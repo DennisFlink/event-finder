fdef0585983799336bf3be3b7ddce7987e0d9f36
DennisFlink <dennis.sabriev@outlook.com>
2024-10-20 19:56:43 +0200

    Refactor import statements in userService.ts

--- event-finder-frontend/src/services/userService.ts
+++ event-finder-frontend/src/services/userService.ts
@@ -1,26 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { IUser } from '../../interface/userTypes';
 
 export const createUser = async (user: IUser) => {
    try {
-      console.log(user);
       const res = await axios.post('http://localhost:3000/api/users', user);
-      if (res.status === 400) {
-         console.error('Email already in use');
-         throw new Error('Email already in use');
-      }
-      if (res.status !== 201) {
-         console.error('Error creating user', res.status);
-         throw new Error('Error creating user');
-      }
       return res.data;
-   } catch (error: any) {
-      if (error.response && error.response.status === 400) {
-         return error.response.data[0].MESSAGE;
-      } else {
-         // For other errors, rethrow them to handle them elsewhere
-         console.error(error);
-         throw error;
+   } catch (error: unknown) {
+      // Check if the error is an AxiosError
+      if (axios.isAxiosError(error)) {
+         if (error.response && error.response.status === 400) {
+            // Return the specific error message from the backend (e.g., 'USER ALREADY EXISTS')
+            return error.response.data[0].MESSAGE;
+         }
       }
+      // For non-Axios errors or other unexpected errors, rethrow them or handle them as needed
+      console.error('An unexpected error occurred:', error);
+      throw error;
    }
 };
