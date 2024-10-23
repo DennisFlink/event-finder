fb2e1a3ba59d9991b9d841edaa81a4e1426a9fd4
skoanton <antonhakansonn@gmail.com>
2024-10-23 10:09:43 +0200

    almost done

--- event-finder-frontend/src/components/Filter.tsx
+++ event-finder-frontend/src/components/Filter.tsx
@@ -1,11 +1,20 @@
+import { FilterIcon } from "lucide-react";
 import FilterForm from "./FilterForm";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 type FilterProps = {};
 
 export default function Filter({}: FilterProps) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <FilterForm />
+      <div>
+        <Button onClick={() => setIsOpen(!isOpen)}>
+          <FilterIcon />
+        </Button>
+        {isOpen && <FilterForm />}
+      </div>
     </>
   );
 }
