afbe23141115710737a0ecf306559fb2b7b459c6
skoanton <antonhakansonn@gmail.com>
2024-10-23 10:09:43 +0200

    almost done

--- event-finder-frontend/src/components/FilterForm.tsx
+++ event-finder-frontend/src/components/FilterForm.tsx
@@ -1,7 +1,8 @@
-import { z } from "zod";
+import { date, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
@@ -13,12 +14,23 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Slider } from "@/components/ui/slider";
-
+import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { CalendarIcon } from "lucide-react";
+import React from "react";
+import { DateRange } from "react-day-picker";
+import { addDays, format } from "date-fns";
+import { Calendar } from "./ui/calendar";
+import { Switch } from "./ui/switch";
 const formSchema = z.object({
   location: z.string().optional(),
   priceRange: z.array(z.number()).length(2).optional(),
-  startDate: z.string().optional(),
-  endDate: z.string().optional(),
+  date: z
+    .object({
+      from: z.date(),
+      to: z.date(),
+    })
+    .optional(),
   author: z.string().optional(),
   ageLimit: z.number().optional(),
   needsApproval: z.boolean().optional(),
@@ -27,13 +39,21 @@ const formSchema = z.object({
 type FilterFormProps = {};
 
 export default function FilterForm({}: FilterFormProps) {
+  const [pickedDateRange, setPickedDateRange] = React.useState<
+    DateRange | undefined
+  >({
+    from: new Date(),
+    to: addDays(new Date(), 1),
+  });
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       location: "",
-      priceRange: [0, 100],
-      startDate: "",
-      endDate: "",
+      priceRange: [0, 9999],
+      date: {
+        from: new Date(),
+        to: addDays(new Date(), 1),
+      },
       author: "",
       ageLimit: 0,
       needsApproval: false,
@@ -43,6 +63,8 @@ export default function FilterForm({}: FilterFormProps) {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
   }
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(9999);
 
   return (
     <Form {...form}>
@@ -69,21 +91,117 @@ export default function FilterForm({}: FilterFormProps) {
               <FormLabel>Price</FormLabel>
               <FormControl>
                 <Slider
-                  defaultValue={[0, 100]}
-                  max={100}
+                  value={[minPrice, maxPrice]}
+                  max={999}
+                  step={10}
+                  minStepsBetweenThumbs={1}
+                  className="p-3"
                   onValueChange={(value) => {
-                    const [min, max] = value;
-                    const newMin = Math.max(0, min);
-                    const newMax = Math.min(100, max);
-                    field.onChange([newMin, newMax]);
+                    setMinPrice(value[0]);
+                    setMaxPrice(value[1]);
                   }}
-                  {...field}
                 />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Date</FormLabel>
+              <FormControl>
+                <div className={cn("grid gap-2")}>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        id="date"
+                        variant={"outline"}
+                        className={cn(
+                          "w-[300px] justify-start text-left font-normal",
+                          !pickedDateRange && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {pickedDateRange?.from ? (
+                          pickedDateRange.to ? (
+                            <>
+                              {format(pickedDateRange.from, "LLL dd, y")} -{" "}
+                              {format(pickedDateRange.to, "LLL dd, y")}
+                            </>
+                          ) : (
+                            format(pickedDateRange.from, "LLL dd, y")
+                          )
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        initialFocus
+                        mode="range"
+                        defaultMonth={pickedDateRange?.from}
+                        selected={pickedDateRange}
+                        onSelect={setPickedDateRange}
+                        numberOfMonths={2}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="author"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Author</FormLabel>
+              <FormControl>
+                <Input placeholder="Search location" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="ageLimit"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Age Limit</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="0" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="needsApproval"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Need approval</FormLabel>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
